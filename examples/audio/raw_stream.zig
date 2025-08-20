c0955070f0dd37b75c46691cc7f8e1cc41a5b6f4
Marcel Ochs <marcel@ochs.rocks>
2025-08-20 17:47:32 +0200

    Update to zig 0.15.0

--- examples/audio/raw_stream.zig
+++ examples/audio/raw_stream.zig
@@ -1,9 +1,11 @@
 // raylib-zig (c) Nikolas Wipper 2023
 
+const std = @import("std");
 const rl = @import("raylib");
 
 const MAX_SAMPLES = 512;
 const MAX_SAMPLES_PER_UPDATE = 4096;
+const C = std.builtin.CallingConvention.c;
 
 const pi = @import("std").math.pi;
 var frequency: f32 = 440;
@@ -11,7 +13,7 @@ var audioFrequency: f32 = 440;
 var oldFrequency: f32 = 1;
 var sineIdx: f32 = 0;
 
-fn audioInputCallback(buffer: ?*anyopaque, frames: c_uint) callconv(.C) void {
+fn audioInputCallback(buffer: ?*anyopaque, frames: c_uint) callconv(C) void {
     audioFrequency = frequency + (audioFrequency - frequency) * 0.95;
 
     const incr = audioFrequency / 44100;
@@ -35,7 +37,7 @@ pub fn main() anyerror!void {
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - raw audio streaming");
     defer rl.closeWindow(); // Close window and OpenGL context
 
-    rl.initAudioDevice();        // Initialize audio device
+    rl.initAudioDevice(); // Initialize audio device
     defer rl.closeAudioDevice(); // Close audio device (music streaming is automatically stopped)
 
     rl.setAudioStreamBufferSizeDefault(MAX_SAMPLES_PER_UPDATE);
@@ -113,10 +115,8 @@ pub fn main() anyerror!void {
 
         rl.clearBackground(.ray_white);
 
-        rl.drawText(rl.textFormat("sine frequency: %i", .{@as(i32, @intFromFloat(frequency))}),
-            rl.getScreenWidth() - 220, 10, 20, .red);
-        rl.drawText("click mouse button to change frequency or pan",
-            10, 10, 20, .dark_gray);
+        rl.drawText(rl.textFormat("sine frequency: %i", .{@as(i32, @intFromFloat(frequency))}), rl.getScreenWidth() - 220, 10, 20, .red);
+        rl.drawText("click mouse button to change frequency or pan", 10, 10, 20, .dark_gray);
 
         // Draw the current buffer state proportionate to the screen
         for (0..screenWidth) |i| {
